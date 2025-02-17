3bca3cee103a93c36f37e4818d5943a3a663de25
GLauzza <77682755+GLauzza@users.noreply.github.com>
2025-02-17 11:19:15 +0100

    WIP Gating

--- hparams/ModelEdit/generate_json.py
+++ hparams/ModelEdit/generate_json.py
@@ -15,14 +15,46 @@ with open(filename, "w") as f:
 
 for i in range(24):
     filename="Qwen_Qwen2.5-0.5B_" + str(i+25) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-5, "n_tok_stop":-5}
+    hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-4, "n_tok_stop":-4}
 
     with open(filename, "w") as f:
         json.dump(hparams, f, indent=2)
 
-for i in range(1, 5):
+for i in range(1, 6):
     filename="Qwen_Qwen2.5-0.5B_" + str(i+48) + ".json"
-    hparams= {"insertion_type":"reccursive", "layer_to_modify":1, "n_tok_start":i, "n_tok_stop":-1}
+    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":-i}
 
     with open(filename, "w") as f:
         json.dump(hparams, f, indent=2)
+
+for i in range(1, 6):
+    filename="Qwen_Qwen2.5-0.5B_" + str(i+53) + ".json"
+    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":0}
+
+    with open(filename, "w") as f:
+        json.dump(hparams, f, indent=2)
+
+for i in range(1, 6):
+    filename="Qwen_Qwen2.5-0.5B_" + str(i+58) + ".json"
+    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":0, "n_tok_stop":i}
+
+    with open(filename, "w") as f:
+        json.dump(hparams, f, indent=2)
+
+for i in range(24):
+    for j in range(11):
+        filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+64) + ".json"
+        hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":j-8}
+
+        with open(filename, "w") as f:
+            json.dump(hparams, f, indent=2)
+
+for i in range(24):
+    for j in range(11):
+        filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+328) + ".json"
+        if j < 9:
+            hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":0}
+        else:
+            hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":0, "n_tok_stop":j-8}
+        with open(filename, "w") as f:
+            json.dump(hparams, f, indent=2)
--- oar.sh
+++ oar.sh
@@ -1,26 +1,45 @@
 #!/bin/bash 
 
 #OAR -q production
+#OAR -p grele
 #OAR -n Evaluate ModelEdit
-#OAR -l host=1/gpu=1, walltime=1
+#OAR -l host=1, walltime=0:10:0
 #OAR -O ./OAR_logs/OAR_%jobid%.out
 #OAR -E ./OAR_logs/OAR_%jobid%.out
 
 export PATH=/home/glauzzana/miniconda3/bin:$PATH
 
 conda activate rome
-module load cuda/12.2.1_gcc-10.4.0
-nvcc --version
-which nvcc
-export CUDA_PATH=/grid5000/spack/v1/opt/spack/linux-debian11-x86_64_v2/gcc-10.4.0/cuda-12.2.1-ihyafmkgj5fmsodkl5bavcwjh564cqhd/bin/nvcc
-module load gcc/10.4.0_gcc-10.4.0
-nvidia-smi
+# module load cuda/12.2.1_gcc-10.4.0
+# nvcc --version
+# which nvcc
+# export CUDA_PATH=/grid5000/spack/v1/opt/spack/linux-debian11-x86_64_v2/gcc-10.4.0/cuda-12.2.1-ihyafmkgj5fmsodkl5bavcwjh564cqhd/bin/nvcc
+# module load gcc/10.4.0_gcc-10.4.0
+# nvidia-smi
 
-# for i in $(seq 25 48)
+# for i in $(seq 25 63) 
 # do
 #     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=50
 # done
-# for i in $(seq -f "%03g" 1 49)
+# for i in $(seq -f "%03g" 1 64)
+# do
+#     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
+# done
+
+# for i in $(seq 64 327)
+# do
+#     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=1
+# done
+# for i in $(seq -f "%03g"  26 289)
+# do
+#     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
+# done
+
+# for i in $(seq 328 591)
+# do
+#     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=1
+# done
+# for i in $(seq -f "%03g"  290 554)
 # do
 #     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
 # done
