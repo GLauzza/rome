f6ebbb191f9b271a1335f9e70de00157bff4d74b
GLauzza <77682755+GLauzza@users.noreply.github.com>
2025-02-21 15:47:51 +0100

    Gating experiments (still not working)

--- hparams/ModelEdit/generate_json.py
+++ hparams/ModelEdit/generate_json.py
@@ -1,60 +1,67 @@
 import os
 import json
 
+# for i in range(24):
+#     filename="Qwen_Qwen2.5-0.5B_" + str(i) + ".json"
+#     hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-1, "n_tok_stop":-1}
+
+#     with open(filename, "w") as f:
+#         json.dump(hparams, f, indent=2)
+
+# filename="Qwen_Qwen2.5-0.5B_" + str(24) + ".json"
+# hparams= {"insertion_type":"reccursive", "layer_to_modify":1, "n_tok_start":-1, "n_tok_stop":-1}
+# with open(filename, "w") as f:
+#     json.dump(hparams, f, indent=2)
+
+# for i in range(24):
+#     filename="Qwen_Qwen2.5-0.5B_" + str(i+25) + ".json"
+#     hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-4, "n_tok_stop":-4}
+
+#     with open(filename, "w") as f:
+#         json.dump(hparams, f, indent=2)
+
+# for i in range(1, 6):
+#     filename="Qwen_Qwen2.5-0.5B_" + str(i+48) + ".json"
+#     hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":-i}
+
+#     with open(filename, "w") as f:
+#         json.dump(hparams, f, indent=2)
+
+# for i in range(1, 6):
+#     filename="Qwen_Qwen2.5-0.5B_" + str(i+53) + ".json"
+#     hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":0}
+
+#     with open(filename, "w") as f:
+#         json.dump(hparams, f, indent=2)
+
+# for i in range(1, 6):
+#     filename="Qwen_Qwen2.5-0.5B_" + str(i+58) + ".json"
+#     hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":0, "n_tok_stop":i}
+
+#     with open(filename, "w") as f:
+#         json.dump(hparams, f, indent=2)
+
+# for i in range(24):
+#     for j in range(11):
+#         filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+64) + ".json"
+#         hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":j-8}
+
+#         with open(filename, "w") as f:
+#             json.dump(hparams, f, indent=2)
+
+# for i in range(24):
+#     for j in range(11):
+#         filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+328) + ".json"
+#         if j < 9:
+#             hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":0}
+#         else:
+#             hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":0, "n_tok_stop":j-8}
+#         with open(filename, "w") as f:
+#             json.dump(hparams, f, indent=2)
+
 for i in range(24):
-    filename="Qwen_Qwen2.5-0.5B_" + str(i) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-1, "n_tok_stop":-1}
+    filename="Qwen_Qwen2.5-0.5B_" + str(i+592) + ".json"
+    hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-3, "n_tok_stop":-3}
 
     with open(filename, "w") as f:
         json.dump(hparams, f, indent=2)
-
-filename="Qwen_Qwen2.5-0.5B_" + str(24) + ".json"
-hparams= {"insertion_type":"reccursive", "layer_to_modify":1, "n_tok_start":-1, "n_tok_stop":-1}
-with open(filename, "w") as f:
-    json.dump(hparams, f, indent=2)
-
-for i in range(24):
-    filename="Qwen_Qwen2.5-0.5B_" + str(i+25) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":-4, "n_tok_stop":-4}
-
-    with open(filename, "w") as f:
-        json.dump(hparams, f, indent=2)
-
-for i in range(1, 6):
-    filename="Qwen_Qwen2.5-0.5B_" + str(i+48) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":-i}
-
-    with open(filename, "w") as f:
-        json.dump(hparams, f, indent=2)
-
-for i in range(1, 6):
-    filename="Qwen_Qwen2.5-0.5B_" + str(i+53) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":-i, "n_tok_stop":0}
-
-    with open(filename, "w") as f:
-        json.dump(hparams, f, indent=2)
-
-for i in range(1, 6):
-    filename="Qwen_Qwen2.5-0.5B_" + str(i+58) + ".json"
-    hparams= {"insertion_type":"single", "layer_to_modify":21, "n_tok_start":0, "n_tok_stop":i}
-
-    with open(filename, "w") as f:
-        json.dump(hparams, f, indent=2)
-
-for i in range(24):
-    for j in range(11):
-        filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+64) + ".json"
-        hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":j-8}
-
-        with open(filename, "w") as f:
-            json.dump(hparams, f, indent=2)
-
-for i in range(24):
-    for j in range(11):
-        filename="Qwen_Qwen2.5-0.5B_" + str(i*11+j+328) + ".json"
-        if j < 9:
-            hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":j-9, "n_tok_stop":0}
-        else:
-            hparams= {"insertion_type":"single", "layer_to_modify":i, "n_tok_start":0, "n_tok_stop":j-8}
-        with open(filename, "w") as f:
-            json.dump(hparams, f, indent=2)
--- oar.sh
+++ oar.sh
@@ -1,27 +1,27 @@
 #!/bin/bash 
 
 #OAR -q production
-#OAR -p grele
+#OAR -p grappe
 #OAR -n Evaluate ModelEdit
-#OAR -l host=1, walltime=0:10:0
+#OAR -l host=1, walltime=0:10
 #OAR -O ./OAR_logs/OAR_%jobid%.out
 #OAR -E ./OAR_logs/OAR_%jobid%.out
 
 export PATH=/home/glauzzana/miniconda3/bin:$PATH
 
 conda activate rome
-# module load cuda/12.2.1_gcc-10.4.0
+# module load cuda/12.2.1_gcc-10.4.0    
 # nvcc --version
 # which nvcc
 # export CUDA_PATH=/grid5000/spack/v1/opt/spack/linux-debian11-x86_64_v2/gcc-10.4.0/cuda-12.2.1-ihyafmkgj5fmsodkl5bavcwjh564cqhd/bin/nvcc
 # module load gcc/10.4.0_gcc-10.4.0
 # nvidia-smi
 
-# for i in $(seq 25 63) 
+# for i in $(seq 0 24) 
 # do
 #     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=50
 # done
-# for i in $(seq -f "%03g" 1 64)
+# for i in $(seq -f "%03g" 1 25)
 # do
 #     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
 # done
@@ -39,7 +39,16 @@ conda activate rome
 # do
 #     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=1
 # done
-# for i in $(seq -f "%03g"  290 554)
+# for i in $(seq -f "%03g"  290 553)
+# do
+#     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
+# done
+
+# for i in $(seq 592 616)
+# do
+#     python3 -m experiments.evaluate --alg_name="ModelEdit" --model_name="Qwen/Qwen2.5-0.5B" --hparams_fname="Qwen_Qwen2.5-0.5B_$i.json" --dataset_size_limit=50
+# done
+# for i in $(seq -f "%03g"  554 577)
 # do
 #     python3 -m experiments.summarize --dir_name="ModelEdit" --runs="run_$i"
 # done
